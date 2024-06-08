0efeb7437f124205ebe7d085a4dd8c839d75884c
Mauricio Girardi <maurigirarde@yahoo.com.br>
2024-06-08 20:24:46 +0100

    feat: create profile route

--- apps/api/src/constants/path-routes.ts
+++ apps/api/src/constants/path-routes.ts
@@ -1,2 +1,3 @@
 export const PATH_SESSIONS_PASSWORD = '/sessions/password'
 export const PATH_USERS = '/users'
+export const PATH_PROFILE = '/profile'
---
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -0,0 +1,52 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { PATH_PROFILE } from '@/constants/path-routes'
+import { prisma } from '@/lib/prisma'
+
+export async function getProfile(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    PATH_PROFILE,
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Get authenticated user profile.',
+        response: {
+          400: z.object({
+            message: z.string().default('User not found.'),
+          }),
+          200: z.object({
+            user: z.object({
+              id: z.string().uuid(),
+              name: z.string().nullable(),
+              email: z.string().email(),
+              avatarUrl: z.string().url().nullable(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { sub } = await request.jwtVerify<{ sub: string }>()
+
+      const user = await prisma.user.findUnique({
+        where: {
+          id: sub,
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          avatarUrl: true,
+        },
+      })
+
+      if (!user) {
+        return reply.status(400).send({ message: 'User not found.' })
+      }
+
+      return reply.status(200).send({ user })
+    },
+  )
+}
