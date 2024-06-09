a4744a856a940c54fb128aa369a227306b983eda
Mauricio Girardi <maurigirarde@yahoo.com.br>
2024-06-09 14:54:43 +0100

    feat: created recover and reset password routes

--- apps/api/src/constants/path-routes.ts
+++ apps/api/src/constants/path-routes.ts
@@ -1,3 +1,5 @@
 export const PATH_SESSIONS_PASSWORD = '/sessions/password'
 export const PATH_USERS = '/users'
 export const PATH_PROFILE = '/profile'
+export const PATH_PASSWORD_RECOVER = '/password/recover'
+export const PATH_PASSWORD_RESET = '/password/reset'
---
+++ apps/api/src/http/routes/auth/request-password-recover.ts
@@ -0,0 +1,48 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { PATH_PASSWORD_RECOVER } from '@/constants/path-routes'
+import { prisma } from '@/lib/prisma'
+
+export async function requestPasswordRecover(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    PATH_PASSWORD_RECOVER,
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Request password recover.',
+        body: z.object({
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { email } = request.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (!userFromEmail) {
+        return reply.status(201).send()
+      }
+
+      const { id: code } = await prisma.token.create({
+        data: {
+          type: 'PASSWORD_RECOVER',
+          userId: userFromEmail.id,
+        },
+      })
+
+      // TODO: send e-mail with password recover link.
+      process.env.NODE_ENV !== 'production' &&
+        console.info('Recover password token:', code)
+
+      return reply.status(201).send()
+    },
+  )
+}
